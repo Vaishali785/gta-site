31caa7654510c35c90c3d51269d69d655250fe0f
Vaishali <dev.vaishali785@gmail.com>
2025-11-26 21:05:33 +0530

    added last step of animation

--- src/components/HeroWithTimeline.tsx
+++ src/components/HeroWithTimeline.tsx
@@ -1,144 +1,12 @@
-import { useGSAP } from "@gsap/react"
-import gsap from "gsap"
 import React from "react"
 
 const HeroWithTimeline = () => {
-	useGSAP(() => {
-		const tl = gsap.timeline({
-			// paused: true,
-			// duration: 10,
-			scrollTrigger: {
-				trigger: "#hero",
-				start: "top top",
-				end: `${window.innerHeight * 3}`,
-				scrub: true,
-				pin: true,
-				markers: true,
-			},
-		})
-		const scaleDownImg = gsap.fromTo(
-			".under-img",
-			{
-				scale: 1.2,
-				ease: "power2.inOut",
-			},
-			{ scale: 1, ease: "power2.inOut" }
-		)
-		const maskLogoImage = gsap.fromTo(
-			".masked-img",
-			{
-				maskSize: "3500%",
-				// maskPosition: "47% 41%",
-			},
-			{
-				maskSize: "16%",
-				// maskPosition: "50% 12%",
-				// opacity: 0.5,
-				background: "white",
-				ease: "power1.inOut",
-				duration: 5,
-			}
-		)
-
-		const maskPositionChange = gsap.fromTo(
-			".masked-img",
-			{
-				maskPosition: "50% 18%",
-			},
-			{
-				maskPosition: "50% 20.4%",
-				ease: "expo.inOut",
-				// duration: 1.5,
-			}
-		)
-
-		const heroImgFadingOpacity = gsap.to(".under-img", {
-			opacity: 0.3,
-			background: "white",
-			ease: "expo.inOut",
-		})
-
-		const heroImgFadingOpacityZero = gsap.to(".under-img", {
-			opacity: 0,
-			background: "white",
-			ease: "expo.inOut",
-		})
-
-		const dateTextReveal = gsap.fromTo(
-			"#comingSoonContent",
-			{
-				opacity: 0,
-				scale: 1.5,
-				backgroundImage: `radial-gradient(circle at 50% 150vh,rgb(255, 212, 128) 0vh,rgb(234, 67, 116) 50vh,rgb(117, 32, 102) 90vh,rgba(32, 31, 66, 0) 100vh)`,
-			},
-			{
-				opacity: 1,
-				scale: 1.25,
-				backgroundImage: `radial-gradient(circle at 50% 125vh,rgb(255, 212, 128) 0vh,rgb(234, 67, 116) 50vh,rgb(117, 32, 102) 90vh,rgba(32, 31, 66, 0) 150vh)`,
-				duration: 2,
-			}
-		)
-
-		const dateTextGradientAnimate = gsap.fromTo(
-			"#comingSoonContent",
-			{
-				scale: 1.25,
-				backgroundImage: `radial-gradient(circle at 50% 100vh,rgb(255, 212, 128) 0vh,rgb(234, 67, 116) 50vh,rgb(117, 32, 102) 90vh,rgba(32, 31, 66, 0) 125vh)`,
-			},
-			{
-				scale: 1,
-				backgroundImage: `radial-gradient(circle at 50% 5vh,rgb(255, 212, 128) 0vh,rgb(234, 67, 116) 50vh,rgb(117, 32, 102) 90vh,rgba(32, 31, 66, 0) 145vh)`,
-				duration: 3,
-			}
-		)
-
-		const revealViImage = gsap.to(".vi-logo-img", {
-			backgroundSize: `clamp(29vh, 35%, 20vh)`,
-			opacity: 1,
-			maskImage: `radial-gradient(
-                circle at 50% 50%,
-                rgb(0, 0, 0) 50%,
-                rgba(0, 0, 0, 0) 140%
-            )`,
-			duration: 1.5,
-		})
-
-		const hideMaskedText = gsap.to(".masked-img", {
-			opacity: 0,
-		})
-
-		const scaleDownViImage = gsap.fromTo(
-			".vi-logo-img",
-			{
-				width: "225px",
-			},
-			{
-				width: "185px",
-				duration: 3,
-				ease: "expo.inOut",
-			}
-		)
-
-		tl.add(scaleDownImg)
-			.add(maskLogoImage, "-=1.5")
-			.add(maskPositionChange, "-=1")
-			.add(heroImgFadingOpacity, "-=2")
-			.add(heroImgFadingOpacityZero, "-=1")
-			.add(dateTextReveal, "-=0.9")
-			.add(dateTextGradientAnimate, "-=0.3")
-			.add(revealViImage, "-=3")
-			.add(hideMaskedText, "-=2.9")
-			.add(scaleDownViImage, "-=2.8")
-	})
-
 	// still lags smooth effect
 	return (
-		<div id="hero-wrapper">
+		<section id="hero-wrapper">
 			<div
 				id="hero"
-				className={`w-full h-screen z-50 flex items-center justify-center`}
-				// style={{ zIndex: zIndex }}
-				// ref={heroRef}
+				className={`w-full h-screen z-40 flex items-center justify-center`}
 			>
 				<div className="relative w-fit h-fit !bg-black">
 					<img
@@ -168,15 +36,18 @@ const HeroWithTimeline = () => {
 				</div>
 
 				<div
-					className="flex flex-col items-center absolute top-[42%] gradient-text scale-150 opacity-0"
+					className="flex flex-col items-center absolute top-[42%] gradient-text transparent-gradient scale-150 opacity-0 poppins-semibold"
 					id="comingSoonContent"
 				>
 					<div className="font-extrabold text-8xl uppercase">Coming</div>
 					<div className="font-extrabold text-8xl uppercase">November 13</div>
 					<div className="font-extrabold text-8xl uppercase">2026</div>
 				</div>
+
+				{/* <IntroText /> */}
 			</div>
-		</div>
+			<div className="black-screen black-gradient w-screen h-screen"></div>
+		</section>
 	)
 }
 
