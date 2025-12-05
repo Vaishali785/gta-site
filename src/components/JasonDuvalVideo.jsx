f32dcc8a7a276d6134ffd2597af2e977dfebc8b0
Vaishali <dev.vaishali785@gmail.com>
2025-12-05 17:00:26 +0530

    added one more video with animations

--- src/components/JasonDuvalVideo.jsx
+++ src/components/JasonDuvalVideo.jsx
@@ -4,14 +4,15 @@ import React, { useRef } from "react"
 
 const JasonDuvalVideo = () => {
 	const videoRef = useRef()
+	const videoRef2 = useRef()
 	useGSAP(() => {
 		const videoTimeline = gsap.timeline({
 			scrollTrigger: {
 				trigger: "#video-section",
-				start: "top top",
-				end: "+=600%  bottom",
+				start: `${window.innerHeight * 3}`,
+				end: `${window.innerHeight * 4.6}`,
 				scrub: true,
-				pin: "#video-section",
+				pin: true,
 				// duration: 5,
 				// markers: true,
 			},
@@ -26,14 +27,14 @@ const JasonDuvalVideo = () => {
 				"-=0.8"
 			)
 			.to(
-				"video",
+				"#video1",
 				{
 					zIndex: 41,
 				},
 				"-=0.8"
 			)
 			.fromTo(
-				"video",
+				"#video1",
 				{
 					filter: "brightness(0.9) blur(10px)",
 				},
@@ -44,17 +45,6 @@ const JasonDuvalVideo = () => {
 				"+=1"
 			)
 
-		// videoRef.current.onloadedmetadata = () => {
-		// 	console.log(">>>here", videoRef.current.duration)
-		// 	videoTimeline.to(
-		// 		videoRef.current,
-
-		// 		{
-		// 			currentTime: videoRef.current.duration,
-		// 			duration: 2,
-		// 		}
-		// 	)
-		// }
 		// Wait for metadata → THEN add video playback tween
 		function addVideoPlayback() {
 			const dur = videoRef.current.duration
@@ -63,15 +53,20 @@ const JasonDuvalVideo = () => {
 				return
 			}
 
-			console.log(">>>hey")
 			// Add AFTER filters and zIndex animation
 			videoTimeline
 				.call(() => videoRef.current.play(), "-=1")
-				.to(videoRef.current, {
-					currentTime: videoRef.current.duration,
-					ease: "none",
-					duration: 2,
-				}) // holds timeline while video plays
+				.fromTo(
+					videoRef.current,
+					{
+						currentTime: 0,
+					},
+					{
+						currentTime: videoRef.current.duration,
+						ease: "none",
+						duration: 3,
+					}
+				) // holds timeline while video plays
 				.call(() => videoRef.current.pause(), "videoPlayLabel")
 
 				.fromTo(
@@ -90,35 +85,118 @@ const JasonDuvalVideo = () => {
 				.to(
 					"#JasonVideo",
 					{
-						bottom: 0,
+						bottom: "100vh",
 						duration: 2,
 					},
 					"videoPlayLabel-=2"
 				)
 
-				.to(
+				.fromTo(
 					"#maskedVideo",
+					{
+						opacity: 1,
+					},
 					{
 						opacity: 0.1,
 						// background: "black",
 					},
 					"videoPlayLabel-=1.5"
 				)
+
+			// .fromTo(
+			// 	"#video1, #JasonVideo",
+			// 	{ zIndex: 41 },
+			// 	{ zIndex: 0 },
+			// 	"videoPlayLabel+=1"
+			// )
+
+			// .to("#video2Section", { zIndex: 41 }, "videoPlayLabel+=1.3")
 		}
 
+		function addVideoPlayback2() {
+			const dur = videoRef2.current.duration
+			if (!dur || isNaN(dur)) {
+				console.warn("Video metadata not ready")
+				return
+			}
+
+			console.log(">>>hey there", dur)
+			// Add AFTER filters and zIndex animation
+			videoTimeline
+				.call(() => videoRef2.current.play(), "-=0.5")
+				.fromTo(
+					videoRef2.current,
+					{
+						currentTime: 0,
+					},
+					{
+						currentTime: videoRef2.current.duration,
+						ease: "none",
+						duration: 1.5,
+					}
+				) // holds timeline while video plays
+				.call(() => videoRef2.current.pause(), "videoPlayLabel2")
+
+				.to(
+					"#video2Section",
+					{
+						opacity: 0,
+						zIndex: 41,
+						position: "fixed",
+						top: 0,
+					},
+					"videoPlayLabel2-=2"
+				)
+				.fromTo(
+					"#video2Section",
+					{
+						opacity: 0,
+						zIndex: -10,
+					},
+					{
+						opacity: 1,
+						zIndex: 41,
+					},
+					"videoPlayLabel2-=1.5"
+				)
+
+				.fromTo(
+					"#video2Text",
+					{
+						opacity: 0,
+						bottom: "5vh",
+					},
+					{
+						opacity: 1,
+						bottom: "8vh",
+						duration: 2,
+					},
+					"videoPlayLabel2-=0.5"
+				)
+
+				.fromTo(
+					"#video-section, #JasonVideo",
+					{
+						opacity: 1,
+						zIndex: 41,
+					},
+					{
+						opacity: 0.5,
+						zIndex: -10,
+						duration: 2,
+					}
+				)
+		}
 		// Metadata already available?
 		if (videoRef.current.readyState >= 1 && !isNaN(videoRef.current.duration)) {
 			addVideoPlayback()
+			addVideoPlayback2()
 		} else {
 			videoRef.current.addEventListener(
 				"loadedmetadata",
-				() => {
-					console.log(
-						">>>>>>>",
-						videoRef.current.readyState,
-						videoRef.current.duration
-					)
-					addVideoPlayback()
+				async () => {
+					await addVideoPlayback()
+					await addVideoPlayback2()
 				},
 
 				{
@@ -130,7 +208,8 @@ const JasonDuvalVideo = () => {
 	return (
 		<section
 			id="video-section"
-			className="w-full h-screen fixed top-full left-0 z-0 mask-video0"
+			className="w-full h-screen fixed top-full left-0 z-0 mask-video0 "
+			// ref=
 		>
 			<div className="w-full h-full mask-video" id="maskedVideo">
 				<video
@@ -140,6 +219,7 @@ const JasonDuvalVideo = () => {
 					playsInline
 					preload="autoplay"
 					ref={videoRef}
+					id="video1"
 					className="w-full h-screen object-cover "
 				/>
 			</div>
@@ -151,7 +231,7 @@ const JasonDuvalVideo = () => {
 					<h2 className="text-[90px] LS-medium text-[#fff9cb] col-start-2 col-span-4 m-0 uppercase tracking-[1px]">
 						Jason Duval
 					</h2>
-					<div className="col-start-2 col-span-4 my-16">
+					<div className="col-start-2 col-span-4 mt-16 mb-28">
 						<p className="text-4xl noto-sans-thaana-medium text-[#ffb0c4] mb-5 ">
 							Jason wants an easy life, but things just keep getting harder.
 						</p>
@@ -165,27 +245,50 @@ const JasonDuvalVideo = () => {
 					<img
 						src="/jason-vid-section/Jason_Duval_01.jpg"
 						alt=""
-						className="col-start-2 col-span-5 aspect-[9/16] object-[80%_center] object-cover"
+						className="col-start-2 col-span-5 aspect-[9/16] object-[80%_center] object-cover hover:border-4 hover:border-[#fff9cb] transition-all duration-300 hover:cursor-pointer"
 					/>
 				</div>
 
-				<div className="video-images grid grid-cols-6 col-start-7 col-span-6 w-full pt-[30vh] gap-8">
-					<div className="w-[calc(100%_+var(--video-padding-inline))] col-start-1 col-span-6 ">
+				<div className="right-side grid grid-cols-6 col-start-7 col-span-6 w-full pt-[20vh] gap-8">
+					<div className="w-[calc(100%_+var(--video-padding-inline))] col-start-1 col-span-6 h-fit">
 						<img
 							src="/jason-vid-section/Jason_Duval_02.jpg"
 							alt=""
-							className="object-cover aspect-square object-[5%_center]"
+							className="object-cover aspect-square object-[5%_center] hover:border-4 hover:border-[#fff9cb] transition-all duration-300 hover:cursor-pointer"
 						/>
 					</div>
-					<div className="col-start-1 col-span-6">
+					<div className="col-start-1 col-span-5">
 						<img
 							src="/jason-vid-section/Jason_Duval_06.jpg"
 							alt=""
-							className="object-cover aspect-square "
+							className="object-cover aspect-square  hover:border-4 hover:border-[#fff9cb] transition-all duration-300 hover:cursor-pointer"
 						/>
 					</div>
 				</div>
 			</div>
+
+			<section
+				id="video2Section"
+				className="w-full h-screen top-[200vh] left-0 -z-10 mask-video0"
+			>
+				<div className="w-full h-full mask-video" id="maskedVideo2">
+					<video
+						src="/videos/output2.mp4"
+						muted
+						// loop
+						playsInline
+						preload="autoplay"
+						ref={videoRef2}
+						className="w-full h-screen object-cover "
+					/>
+				</div>
+				<q
+					id="video2Text"
+					className="text-[70px] LS-medium text-[#fff9cb] col-start-2 col-span-4 m-0 uppercase tracking-[2px] absolute bottom-[5vh] left-1/5 w-1/3 leading-[0.9] opacity-0"
+				>
+					If anything happens, I'm right behind you.
+				</q>
+			</section>
 		</section>
 	)
 }
