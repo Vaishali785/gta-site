f948e796f3a6c8a1330e289b37226e0775e93dd1
Vaishali <dev.vaishali785@gmail.com>
2025-12-17 22:53:37 +0530

    navbar and logo component

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,16 @@
+import React from "react"
+import Logo from "./Logo"
+
+const Navbar = () => {
+	return (
+		<div className="w-full fixed top-1/12 px-16 flex justify-between z-[100]">
+			<Logo />
+			<div className="flex flex-col gap-1 items-center justify-evenly">
+				<div className="w-7 h-1.5 bg-white"></div>
+				<div className="w-7 h-1.5 bg-white"></div>
+			</div>
+		</div>
+	)
+}
+
+export default Navbar
