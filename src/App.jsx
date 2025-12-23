fdf8087a4fe1b353bb7de3d6cba7ecf31c3b95ca
Vaishali <dev.vaishali785@gmail.com>
2025-12-23 21:12:23 +0530

    app file with lenis

--- src/App.jsx
+++ src/App.jsx
@@ -14,43 +14,14 @@ import RaulBautistaIntro from "./components/RaulBautistaIntro"
 gsap.registerPlugin(ScrollTrigger)
 function App() {
 	const lenis = useLenis()
-	// called every scroll
-	// console.log(lenis.velocity, lenis.limit, lenis.progress)
-	// })
-
-	// useEffect(() => {
-	// 	function update(time) {
-	// 		lenis.current?.lenis?.raf(time)
-	// 	}
-
-	// 	const rafId = requestAnimationFrame(update)
-
-	// 	return () => cancelAnimationFrame(rafId)
-	// }, [])
-	// const masterTimeline = useMasterTimeline()
-
-	// useLayoutEffect(() => {
-	// 	masterTimeline.add(heroSection())
-	// 	masterTimeline.add(firstVideoSection(), "-=2.5")
-	// 	const st = ScrollTrigger.create({
-	// 		// paused: true,
-	// 		animation: masterTimeline,
-	// 		trigger: "#wrapper",
-	// 		start: "top top",
-	// 		end: window.innerHeight * 5,
-	// 		scrub: true,
-	// 		pin: true,
-	// 		markers: true,
-	// 	})
-
-	// 	return () => st.kill()
-	// }, [masterTimeline])
 
 	return (
-		// <MasterTlContext.Provider value={masterTl.current}>
 		<div className="relative overflow-hidden" id="wrapper">
-			<ReactLenis root options={{ lerp: 0.2, smoothWheel: true }} />
-			{/* <Hero2 zIndex={50} /> */}
+			<ReactLenis
+				root
+				options={{ lerp: 0.2, smoothWheel: true, syncTouchLerp: 0.2 }}
+			/>
+
 			<Navbar />
 			<HeroWithBigImg />
 			<IntroText />
@@ -59,10 +30,7 @@ function App() {
 			<ClippedImageSection />
 			<RaulBautistaIntro />
 			<ClosingSection />
-			{/* <Footer /> */}
-			{/* <div className="h-[1440px] w-full bg-red-400"></div> */}
 		</div>
-		// </MasterTlContext.Provider>
 	)
 }
 
