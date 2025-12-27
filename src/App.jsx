23dbbf11a8f1e6462b4a953aadbc17b73cc9622e
Vaishali <dev.vaishali785@gmail.com>
2025-12-27 17:11:01 +0530

    Added loading while assets not loaded

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import gsap from "gsap"
 import { ScrollTrigger } from "gsap/all"
 import { ReactLenis } from "lenis/react"
-import { Suspense, useEffect, useState } from "react"
+import { useLayoutEffect, useState } from "react"
 import ClippedImageSection from "./components/ClippedImageSection"
 import ClosingSection from "./components/ClosingSection"
 import HeroWithBigImg from "./components/HeroWithBigImg"
@@ -12,24 +12,31 @@ import Loading from "./components/Loading"
 import Navbar from "./components/Navbar"
 import RaulBautistaIntro from "./components/RaulBautistaIntro"
 
-// const JasonDuvalVideo = lazy(() => import("./components/JasonDuvalVideo"))
-// const JasonWithGunVideo = lazy(() => import("./components/JasonWithGunVideo"))
-// const RaulBautistaIntro = lazy(() => import("./components/RaulBautistaIntro"))
-// const ClippedImageSection = lazy(() =>
-// 	import("./components/ClippedImageSection")
-// )
-// const ClosingSection = lazy(() => import("./components/ClosingSection"))
-
-// export const MasterTlContext = createContext(null)
 gsap.registerPlugin(ScrollTrigger)
 function App() {
 	const [loaded, setLoaded] = useState(false)
 
-	useEffect(() => {
-		const img = new Image()
-		img.src = "/hero/heroImg.webp"
+	useLayoutEffect(() => {
+		const images = Array.from(document.images)
+		const videos = Array.from(document.querySelectorAll("video"))
 
-		img.onload = () => setLoaded(true)
+		const fontPromises = document.fonts.ready
+
+		const imgPromises = images.map((img) =>
+			img.complete
+				? Promise.resolve()
+				: new Promise((res) => img.addEventListener("load", res))
+		)
+
+		const videoPromises = videos.map((v) =>
+			v.readyState >= 2
+				? Promise.resolve()
+				: new Promise((res) => v.addEventListener("loadedmetadata", res))
+		)
+
+		Promise.all([...imgPromises, ...videoPromises, fontPromises]).then(() => {
+			setLoaded(true)
+		})
 	}, [])
 
 	if (!loaded) return <Loading />
@@ -39,18 +46,14 @@ function App() {
 				root
 				options={{ lerp: 0.2, smoothWheel: true, syncTouchLerp: 0.2 }}
 			/>
-			{/* <Suspense fallback={<Loading />}> */}
 			<Navbar />
 			<HeroWithBigImg />
 			<IntroText />
-			<Suspense fallback={null}>
-				<JasonDuvalVideo />
-				<JasonWithGunVideo />
-				<ClippedImageSection />
-				<RaulBautistaIntro />
-				<ClosingSection />
-			</Suspense>
-			{/* </Suspense> */}
+			<JasonDuvalVideo />
+			<JasonWithGunVideo />
+			<ClippedImageSection />
+			<RaulBautistaIntro />
+			<ClosingSection />
 		</div>
 	)
 }
